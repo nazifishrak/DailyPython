54d62f7888cdfed95d57a023b61ee6dd87244f7c
Nazif <nzfishrak60@gmail.com>
2022-07-09 00:15:39 +0600

    Added an alternate function to test a prime number

--- Day7PrimeNumberChecker.py
+++ Day7PrimeNumberChecker.py
@@ -3,11 +3,22 @@ inp = int(input("Check this number: "))
 def prime_checker(number = inp) -> None:
     for i in range(2,number+1):
         if number%i==0 and (not(number == i)):
-            print("Not Prime")
+            print(f"The number {number} is Composite")
             break
         else:
             if (i==number):
-                print("prime")
+                print(f"The number {number} is Prime")
+
+def prime_checker_alternate(number = inp) -> None:
+    is_prime = True
+    for i in range(2,number):
+        if number % i ==0:
+            is_prime = False
+    if is_prime:
+        print(f"The number {number} is Prime")
+    else:
+        print(f"The number {number} is Composite")
 
         
-prime_checker()
+# prime_checker()
+prime_checker_alternate()
