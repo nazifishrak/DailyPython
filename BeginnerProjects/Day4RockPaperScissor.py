d306566b05e5ea98aab7bb7b79971954f6c1310d
nazifishrak <nzfishrak60@gmail.com>
2022-09-26 19:26:05 -0700

    Just for fun

--- BeginnerProjects/Day4RockPaperScissor.py
+++ BeginnerProjects/Day4RockPaperScissor.py
@@ -1,3 +1,4 @@
+from random import randint as randint
 rock = '''
     _______
 ---'   ____)
@@ -25,24 +26,22 @@ scissors = '''
 ---.__(___)
 '''
 
-from random import randint as randint
-moves=[rock,paper,scissors]
-random_number = randint(0,2)
+moves = [rock, paper, scissors]
+random_number = randint(0, 2)
 inp = int(input("Choose 0 for rock, 1 for paper, 2 for scissor: "))
 print(f"You chose {moves[inp]}")
 print(f"Computer chose {moves[random_number]}")
 
 if inp == random_number:
     print("Tie")
-elif inp==0 and random_number == 1:
+elif inp == 0 and random_number == 1:
     print("Computer won")
 
-elif inp==1 and random_number == 2:
+elif inp == 1 and random_number == 2:
     print("Computer won")
 
-elif inp==2 and random_number == 0:
+elif inp == 2 and random_number == 0:
     print("Computer won")
 
 else:
-    print("You won")
-
+    print("You won!!")
